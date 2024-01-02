c24e07be7dd931862cc85d358b8bdec28cdc0fb2
jainefer <jy5849@naver.com>
2024-01-02 22:26:51 +0900

    fetch kakao api 추가

---
+++ src/routes/kakao.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { kakaoController } from "./controllers/kakaoController.js";
+
+const oauthRouter = express.Router();
+
+kakaoRouter.post(" ",KakaoController);
+
+export default kakaoRouter;
